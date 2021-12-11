d5cfeaf82812fb53877f1ae1fdf5896021d4bbb5
wendyyanto <yanto.wendy98@gmail.com>
2021-12-11 16:17:01 +0700

    session 19

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -4,28 +4,43 @@ import { useEffect, useState } from 'react'
 import Text from './components/text'
 
 function App() {
-  const [info, setInfo] = useState(10)
+  const [info, setInfo] = useState('Ini Saya')
   const [color, setColor] = useState('red')
-  const [intervalId, setIntervalId] = useState(0)
 
-  // 1s = 1000ms
-  const id = setInterval(() => {
-    const newInfo = info - 1
-    setInfo(newInfo)
-    setColor('blue')
+  const [loading, setLoading] = useState(true)
 
-    clearTimeout(id)
-    setIntervalId(id)
-  }, 1000)
+  setTimeout(() => {
+    setLoading(false)
+  }, 2000)
+
+  // setTimeout(() => {
+  //   setInfo('Tulisan lain')
+  // }, 5000)
+
+  function loadComponent() {
+    if (loading) {
+      return <div>Loading...</div>
+    } else {
+      return <Text content={info} color={color} backgroundColor="green" />
+    }
+  }
+
+  // IF 1
+
+  // if (a) {
+  //   b
+  // } else {
+  //   c
+  // }
+
+  // IF 2
+  // a ? b : c
 
   return (
     <div>
-      <Text content={info} color={color} backgroundColor="green" />
-      {/* 
-      <Text content="Halo 2" color="green" backgroundColor="blue" />
-      <Text content="Halo 3" color="blue" backgroundColor="red" />
-      <Text content="Halo 4" /> 
-      */}
+      {
+        loadComponent()
+      }
     </div>
   )
 }
--- react/my-app/src/components/text.js
+++ react/my-app/src/components/text.js
@@ -1,12 +1,23 @@
 // import './text.css'
 
+import { useEffect } from "react"
+
 function Text(props) {
   const content = props.content
   const color = props.color || 'red'
   const backgroundColor = props.backgroundColor || 'black'
 
+  useEffect(() => {
+    // mounting
+    console.log('Text is mounted')
+
+    return () => {
+      console.log('Text is unmounted')
+    }
+  }, [])
+
   return (
-    <div className="text" style={{ color, backgroundColor }}>
+    <div className="text" style={{ color, backgroundColor, fontSize:'50px' }}>
       { content }
     </div>
   )
